fd16b74fa8e1ddc208453821b617c5c2b2549824
ajacker <ajacker@foxmail.com>
2019-12-13 12:30:48 +0800

    201509-4 高速公路,把tarjan改成非递归（模拟栈），满分

--- src/csp_201509_4.java
+++ src/csp_201509_4.java
@@ -1,7 +1,11 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.io.StreamTokenizer;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
 
 /**
  * @author ajacker
@@ -9,25 +13,30 @@ import java.util.*;
  */
 public class csp_201509_4 {
     static List<Integer>[] map;
-    static boolean[] visited;
+    static boolean[] inStack;
     static boolean[][] canReach;
     static int n, m, index;
     static int count = 0;
     static int[] dfn, low;
     static Stack<Integer> helper;
+    static StreamTokenizer in = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
 
-    public static void main(String[] args) {
-        n = InputReader.nextInt();
-        m = InputReader.nextInt();
-        visited = new boolean[n + 1];
+    public static void main(String[] args) throws IOException {
+        in.nextToken();
+        n = (int) in.nval;
+        in.nextToken();
+        m = (int) in.nval;
+        inStack = new boolean[n + 1];
         map = new LinkedList[m + 1];
 
         for (int i = 1; i <= m; i++) {
             map[i] = new LinkedList<>();
         }
         for (int i = 1; i <= m; i++) {
-            int u = InputReader.nextInt();
-            int v = InputReader.nextInt();
+            in.nextToken();
+            int u = (int) in.nval;
+            in.nextToken();
+            int v = (int) in.nval;
             map[u].add(v);
         }
         index = 0;
@@ -35,8 +44,8 @@ public class csp_201509_4 {
         dfn = new int[n + 1];
         low = new int[n + 1];
         for (int i = 1; i <= n; i++) {
-            if (!visited[i]) {
-                tarjan(i);
+            if (dfn[i] == 0) {
+                tarjan2(i);
             }
         }
 //        tarjan();
@@ -44,70 +53,81 @@ public class csp_201509_4 {
 
     }
 
-    private static void tarjan() {
-        for (int i = 1; i <= n; i++) {
-            if (!visited[i]) {
-                Stack<Integer> stack = new Stack<>();
-                stack.push(i);
-                while (!stack.isEmpty()) {
-                    int cur = stack.peek();
-                    visited[cur] = true;
-                    dfn[cur] = low[cur] = ++index;
-                    helper.push(cur);
-                    int j;
-                    for (j = 0; j < map[cur].size(); j++) {
-                        int next = map[cur].get(j);
-                        if (!visited[next]) {
-                            stack.push(next);
-                            break;
-                        } else if (helper.contains(next)) {
-                            low[cur] = Math.min(low[cur], dfn[next]);
-                        }
+    private static void tarjan2(int u) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(u);
+        while (!stack.isEmpty()) {
+            int cur = stack.peek();
+            if (dfn[cur] == 0) {
+                dfn[cur] = low[cur] = ++index;
+                helper.push(cur);
+                inStack[cur] = true;
+            }
+            int i = 0;
+            boolean flag = false;
+            while (i < map[cur].size()) {
+                int v = map[cur].get(i);
+                if (dfn[v] == 0) {
+                    stack.push(v);
+                    flag = true;
+                    break;
+                } else if (inStack[v]) {
+                    low[cur] = Math.min(low[cur], dfn[v]);
+                }
+                i++;
+            }
+            if (flag) {
+                continue;
+            }
+            if (i == map[cur].size()) {
+                int v = stack.pop();
+                if (dfn[cur] == low[cur]) {
+                    int c = 0, next;
+                    //计算有多少个点（到最后一个点为止）
+                    do {
+                        next = helper.pop();
+                        inStack[next] = false;
+                        c++;
+                    } while (cur != next);
+                    //统计有多少对
+                    if (c > 0) {
+                        count += c * (c - 1) / 2;
                     }
-                    if (j == map[cur].size()) {
-                        int v = stack.pop();
-                        try {
-                            int u = stack.peek();
-                            low[u] = Math.min(low[u], low[v]);
-                            if (dfn[u] == low[u]) {
-                                int cnt = 0;
-                                do {
-                                    v = helper.pop();
-                                    cnt++;
-                                } while (u != v);
-                                if (cnt > 0) {
-                                    count += cnt * (cnt - 1) / 2;
-                                }
-                            }
-                        } catch (Exception ignored) {
-                        }
-                    }
-
+                }
+                if (!stack.isEmpty()) {
+                    low[stack.peek()] = Math.min(low[stack.peek()], low[v]);
                 }
             }
         }
     }
 
+
     private static void tarjan(int u) {
-        visited[u] = true;
         dfn[u] = low[u] = ++index;
         helper.push(u);
+        inStack[u] = true;
         int v;
         for (int i = 0; i < map[u].size(); i++) {
             v = map[u].get(i);
-            if (!visited[v]) {
+            if (dfn[u] == 0) {
                 tarjan(v);
+                // //回溯时更新low[ ]，取最小值
                 low[u] = Math.min(low[u], low[v]);
-            } else if (helper.contains(v)) {
+            } else if (inStack[v]) {
+                //一旦遇到已入栈的点，就将该点作为连通量的根
                 low[u] = Math.min(low[u], dfn[v]);
             }
         }
+        //得到一个强联通分量
         if (dfn[u] == low[u]) {
             int c = 0;
+            //计算有多少个点（到最后一个点为止）
             do {
                 v = helper.pop();
+                inStack[v] = false;
                 c++;
             } while (u != v);
+            //统计有多少对
             if (c > 0) {
                 count += c * (c - 1) / 2;
             }
@@ -118,10 +138,10 @@ public class csp_201509_4 {
     private static void dfs() {
         canReach = new boolean[n + 1][n + 1];
         for (int i = 1; i <= n; i++) {
-            Arrays.fill(visited, false);
+            Arrays.fill(inStack, false);
             Stack<Integer> stack = new Stack<>();
             stack.push(i);
-            visited[i] = true;
+            inStack[i] = true;
             while (!stack.empty()) {
                 int cur = stack.pop();
                 if (i != cur) {
@@ -131,9 +151,9 @@ public class csp_201509_4 {
                     }
                 }
                 for (Integer next : map[cur]) {
-                    if (!visited[next]) {
+                    if (!inStack[next]) {
                         stack.push(next);
-                        visited[next] = true;
+                        inStack[next] = true;
                     }
                 }
             }
@@ -141,23 +161,5 @@ public class csp_201509_4 {
         }
     }
 
-    static class InputReader {
-        static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        static StringTokenizer tokenizer = null;
 
-        static String next() {
-            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
-                try {
-                    tokenizer = new StringTokenizer(reader.readLine());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return tokenizer.nextToken();
-        }
-
-        static int nextInt() {
-            return Integer.parseInt(next());
-        }
-    }
 }
