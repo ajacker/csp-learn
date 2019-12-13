727dd791565a1a53cb48fda7b82a394750735dd4
ajacker <ajacker@foxmail.com>
2019-12-13 16:07:05 +0800

    201712-4 优化了10分，原因是int装不下(90)

--- src/csp_201712_4.java
+++ src/csp_201712_4.java
@@ -1,26 +1,36 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StreamTokenizer;
 
 /**
  * @author ajacker
  * 最短路径 迪杰斯特拉
  */
 public class csp_201712_4 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+    static StreamTokenizer in = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+
+    public static void main(String[] args) throws IOException {
+        in.nextToken();
+        int n = (int) in.nval;
+        in.nextToken();
+        int m = (int) in.nval;
         //载入邻接矩阵存储的图
         Road[][] map = new Road[n + 1][n + 1];
         for (int i = 1; i <= m; i++) {
-            int t = sc.nextInt();
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            int c = sc.nextInt();
+            in.nextToken();
+            int t = (int) in.nval;
+            in.nextToken();
+            int a = (int) in.nval;
+            in.nextToken();
+            int b = (int) in.nval;
+            in.nextToken();
+            double c = in.nval;
             Road road = new Road(t, c);
             map[a][b] = road;
             map[b][a] = road;
         }
-        System.out.println(dijkstra(n, map));
+        System.out.printf("%.0f", dijkstra(n, map));
     }
 
     /**
@@ -30,7 +40,7 @@ public class csp_201712_4 {
      * @param map 邻接矩阵存储的图
      * @return 最小疲劳值
      */
-    private static int dijkstra(int n, Road[][] map) {
+    private static double dijkstra(int n, Road[][] map) {
         // 定义一维数组用来存储v1到每个点的最短路径，找到比原来更短的则直接覆盖
         Route[] paths = new Route[n + 1];
         //保存是否访问过
@@ -41,14 +51,14 @@ public class csp_201712_4 {
             if (map[1][i] != null) {
                 route.add(map[1][i]);
             } else {
-                route.tired = 1000000;
+                route.tired = Double.MAX_VALUE;
             }
             paths[i] = route;
         }
         isVisited[1] = true;
         for (int i = 2; i <= n; i++) {
             // 在已经存在的路径中找到一条未被访问且最短的路径
-            int min = Integer.MAX_VALUE;
+            double min = Double.MAX_VALUE;
             int minIndex = -1;
             for (int j = 2; j <= n; j++) {
                 if (!isVisited[j] && paths[j].tired < min) {
@@ -80,9 +90,9 @@ public class csp_201712_4 {
         /**
          * 道路长度
          */
-        int length;
+        double length;
 
-        Road(int type, int length) {
+        Road(int type, double length) {
             this.type = type;
             this.length = length;
         }
@@ -97,11 +107,11 @@ public class csp_201712_4 {
         /**
          * 连续小道的长度
          */
-        int littleRoad;
+        double littleRoad;
         /**
          * 疲劳度
          */
-        int tired;
+        double tired;
 
         Route() {
         }
@@ -119,7 +129,7 @@ public class csp_201712_4 {
          */
         Route add(Road road) {
             if (road == null) {
-                tired += 1000000;
+                tired = Double.MAX_VALUE;
                 return this;
             }
             if (road.type == 0) {
@@ -127,7 +137,7 @@ public class csp_201712_4 {
                 littleRoad = 0;
             } else {
                 //除了本节点外的连续小道长度
-                int prevLittleRoad = littleRoad;
+                double prevLittleRoad = littleRoad;
                 //到目前为止的连续小道长度
                 littleRoad += road.length;
                 tired = tired - prevLittleRoad * prevLittleRoad + littleRoad * littleRoad;
@@ -141,21 +151,22 @@ public class csp_201712_4 {
          * @param road 路径
          * @return 假设添加这段路径后的疲劳值
          */
-        int addPrev(Road road) {
+        double addPrev(Road road) {
             if (road == null) {
-                return 1000000;
+                return Double.MAX_VALUE;
             }
-            int temp = tired;
+            double temp = tired;
             if (road.type == 0) {
                 temp += road.length;
             } else {
                 //除了本节点外的连续小道长度
-                int prevLittleRoad = littleRoad;
+                double prevLittleRoad = littleRoad;
                 //到目前为止的连续小道长度
-                int tmp = littleRoad + road.length;
+                double tmp = littleRoad + road.length;
                 temp = temp - prevLittleRoad * prevLittleRoad + tmp * tmp;
             }
             return temp;
         }
+
     }
 }
